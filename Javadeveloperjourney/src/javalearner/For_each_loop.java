5d54966314e30a5dc58b4cc433f60130f6a0acb9
varadhanbnm61 <varadhanbnm6@gmail.com>
2023-08-17 01:14:51 +0530

    varadhan

---
+++ Javadeveloperjourney/src/javalearner/For_each_loop.java
@@ -0,0 +1,17 @@
+package javalearner;
+
+public class For_each_loop {
+
+	public static void main(String[] args) {
+		
+		
+		int a[]= { 10,20,30,40,50};
+		
+		for(int b : a) {
+			
+			System.out.print(b+" ");
+		}
+
+	}
+
+}
